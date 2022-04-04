a237eaf32b20c7f15627d5e28982d206349423ec
Samuel Bretton <c0026228@my.shu.ac.uk>
2022-04-04 20:25:03 +0000

    fixed patient and surgery relationship after unit testing

--- appointmentsystem/grails-app/domain/com/SBretton/Patient.groovy
+++ appointmentsystem/grails-app/domain/com/SBretton/Patient.groovy
@@ -1,29 +1,29 @@
 package com.SBretton
 
 class Patient {
-    String name
-    String address
-    String residence
+    String patientName
+    String patientAddress
+    String patientResidence
     Date patientDob
     int patientID
     Date dateregistered
     String patientPhone
         String toString(){
-        return name
+        return patientName
     } 
 
 
 
     static constraints = {
-        name nullable:false, blank:false
-        address nullable:false, blank:false, unique: true
-        residence nullable:false, blank:false
+        patientName nullable:false, blank:false
+        patientAddress nullable:false, blank:false
+        patientResidence nullable:false, blank:false
         patientDob nullable:false, blank:false
-        patientID nullable:false, blank:false, unique: true
+        patientID nullable:false, blank:false
         dateregistered nullable:false, blank:false
-        patientPhone nullable:false, blank:false, unique: true, size: 0..11
+        patientPhone nullable:false, blank:false
         //thepatient nullable:false, blank:false
     }
-static hasMany = [doctors:Doctor, appointments:Appointment, prescriptions:Prescription, surgerys:Surgery]
-static belongsTo = [Doctor, Surgery, Appointment, Prescription]
+static hasMany = [doctor:Doctor, surgery:Surgery, prescription:Prescription, appointment:Appointment]
+static belongsTo = [doctor:Doctor,surgery:Surgery, appointment:Appointment]
 }
